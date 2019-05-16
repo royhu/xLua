00e1e22faf8f08062f049ba0060397ea777b9542
blus <blus007@126.com>
2019-05-16 19:30:08 +0800

    添加strip path

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -10,7 +10,7 @@ echo make armeabi-v7a ================================================
 STRIP_PATH=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip
 
 mkdir -p build_v7a && cd build_v7a
-cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.6 -DANDROID_NATIVE_API_LEVEL=android-9 ../
+cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.6 -DANDROID_NATIVE_API_LEVEL=android-9 -DANDROID_STRIP_EXEC=$STRIP_PATH ../
 cd ..
 cmake --build build_v7a --config Release
 mkdir -p plugin_lua53/Plugins/Android/libs/armeabi-v7a/
@@ -24,7 +24,7 @@ echo make x86 ================================================
 STRIP_PATH=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-strip
 
 mkdir -p build_x86 && cd build_x86
-cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 ../
+cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 -DANDROID_STRIP_EXEC=$STRIP_PATH ../
 cd ..
 cmake --build build_x86 --config Release
 mkdir -p plugin_lua53/Plugins/Android/libs/x86/
