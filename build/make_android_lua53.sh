91f7ed106f834a2ae9d8078cb148a0243ace546b
blus <blus007@126.com>
2019-05-16 18:58:13 +0800

    添加自动strip so

--- build/make_android_lua53.sh
+++ build/make_android_lua53.sh
@@ -2,12 +2,26 @@ if [ -z "$ANDROID_NDK" ]; then
     export ANDROID_NDK=~/android-ndk-r10e
 fi
 
+echo make armeabi-v7a ================================================
+# linux
+# STRIP_PATH=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-strip
+
+# mac
+STRIP_PATH=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-strip
+
 mkdir -p build_v7a && cd build_v7a
 cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang3.6 -DANDROID_NATIVE_API_LEVEL=android-9 ../
 cd ..
 cmake --build build_v7a --config Release
 mkdir -p plugin_lua53/Plugins/Android/libs/armeabi-v7a/
 cp build_v7a/libxlua.so plugin_lua53/Plugins/Android/libs/armeabi-v7a/libxlua.so
+
+echo make x86 ================================================
+# linux
+# STRIP_PATH=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-strip
+
+# mac
+STRIP_PATH=$ANDROID_NDK/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-strip
 
 mkdir -p build_x86 && cd build_x86
 cmake -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang3.5 -DANDROID_NATIVE_API_LEVEL=android-9 ../
