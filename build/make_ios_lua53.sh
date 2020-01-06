9bae34b0ed21c8f864f8a232c92d79431f033494
halx99 <halx99@live.com>
2020-01-06 20:24:10 +0800

    Copy all .a files to plugins for ios

--- build/make_ios_lua53.sh
+++ build/make_ios_lua53.sh
@@ -3,5 +3,9 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.c
 cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_lua53/Plugins/iOS/
-cp build_ios/Release-iphoneos/*.a plugin_lua53/Plugins/iOS/*.a
+cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a
+cp build_ios/zlib/Release-iphoneos/libz.a plugin_lua53/Plugins/iOS/libz.a
+
+# pitfall: libcares.a require libresolve.9.tbd, just add it to Framework
+cp build_ios/c-ares/Release-iphoneos/libcares.a plugin_lua53/Plugins/iOS/libcares.a
 
