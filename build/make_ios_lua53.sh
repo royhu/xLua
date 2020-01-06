34e61cd21cfd98e1e305ae1470ccaed83351eca7
halx99 <halx99@live.com>
2020-01-06 20:20:19 +0800

    Copy all .a files to plugins for ios

--- build/make_ios_lua53.sh
+++ build/make_ios_lua53.sh
@@ -3,5 +3,5 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.c
 cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_lua53/Plugins/iOS/
-cp build_ios/Release-iphoneos/libxlua.a plugin_lua53/Plugins/iOS/libxlua.a 
+cp build_ios/Release-iphoneos/*.a plugin_lua53/Plugins/iOS/*.a
 
