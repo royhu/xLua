c07cdf7906a545e8da1cfe2c125a31e79a539f77
halx99 <halx99@live.com>
2020-04-02 12:14:36 +0800

    Use vs2019 to build luac

--- build/luac/make_win32.bat
+++ build/luac/make_win32.bat
@@ -1,6 +1,6 @@
 mkdir build32 & pushd build32
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 14 2015" ..
-IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 15 2017" ..
+cmake -DLUAC_COMPAT_FORMAT=ON -DLUAC_COMPAT_32BIT=ON -A Win32 ..
+
 popd
 cmake --build build32 --config Release
 pause
--- build/luac/make_win64.bat
+++ build/luac/make_win64.bat
@@ -1,6 +1,5 @@
 mkdir build64 & pushd build64
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 14 2015 Win64" ..
-IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 15 2017 Win64" ..
+cmake -DLUAC_COMPAT_FORMAT=ON -DLUAC_COMPAT_32BIT=ON ..
 popd
 cmake --build build64 --config Release
 pause
